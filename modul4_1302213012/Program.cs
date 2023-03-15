7948afb1f487b24e03d14881120ad183a477d407
bimozachri <bimozachri@gmail.com>
2023-03-15 16:12:41 +0700

    menambahkan class KodeBuah

--- modul4_1302213012/Program.cs
+++ modul4_1302213012/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public enum buah
+    {
+        Apel,
+        Aprikot,
+        Alpukat,
+        Pisang,
+        Paprika,
+        Blackberry,
+        Ceri,
+        Kelapa,
+        Jagung
+    }
+
+    public class KodeBuah
+    {
+        public static string getKodeBuah(buah inBuah)
+        {
+            string[] outKodeBuah = { "A00", "B00", "C00", "D00", "E00", "K00", "L00", "M00", "N00", "O00" };
+            int inInt = (int)inBuah;
+            return outKodeBuah[inInt];
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        KodeBuah Buah = new KodeBuah();
+        buah enBuah = buah.Alpukat;
+        string codeBuah = KodeBuah.getKodeBuah(enBuah);
+        Console.WriteLine("Buah " + enBuah + " memiliki kode " + codeBuah);
+    }
+}
